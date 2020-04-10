41130c8a90caf4207b9f3650aaada0133fc93afc
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-04-10 13:25:40 -0400

    Added telerik example with theme chooser

---
+++ Telerik/TelerikBlazorApp1/Client/Services/WeatherForecastService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TelerikBlazorApp1.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace TelerikBlazorApp1.Services
+{
+    public class WeatherForecastService
+    {
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        public WeatherForecastService(HttpClient client)
+        {
+            Http = client;
+        }
+
+        public async Task<List<WeatherForecast>> GetForecastListAsync(DateTime startDate)
+        {
+            var data = await Http.GetJsonAsync<List<WeatherForecast>>("WeatherForecast?startDate=" + startDate.ToString(CultureInfo.InvariantCulture));
+            return data;
+        }
+
+        public async Task UpdateForecastAsync(WeatherForecast forecastToUpdate)
+        {
+            await Http.PostJsonAsync("WeatherForecast", forecastToUpdate);
+        }
+
+        public async Task DeleteForecastAsync(WeatherForecast forecastToRemove)
+        {
+            await Http.DeleteAsync("WeatherForecast?idToRemove=" + forecastToRemove.Id);
+        }
+
+        public async Task InsertForecastAsync(WeatherForecast forecastToInsert)
+        {
+            await Http.PutJsonAsync("WeatherForecast", forecastToInsert);
+        }
+    }
+}
---
+++ Telerik/TelerikBlazorApp1/Shared/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelerikBlazorApp1.Shared
+{
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public List<MenuItem> Items { get; set; }
+    }
+}
